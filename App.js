b14eae81ae95fcd157168b38cc737c4e0c7d17f5
Brian Bouchard <b.bouch@gmail.com>
2020-05-25 21:05:12 -0600

    Setup redux store

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+import productsReducer from './store/reducers/products';
+
+const rootReducer = combineReducers({
+  products: productsReducer,
+});
+
+const store = createStore(rootReducer);
 
 export default function App() {
-  return (
-    <View>
-      <Text>A New App!</Text>
-    </View>
-  );
+  return
+  <Provider store={store}>
+    <View>...</View>
+  </Provider>;
 }
