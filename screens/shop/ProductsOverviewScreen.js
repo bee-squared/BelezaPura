f94f63219db226e5a48b970936dee5966bf7686e
Brian Bouchard <b.bouch@gmail.com>
2020-05-26 22:14:04 -0600

    Set up flatlist in products overview screen

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,8 +1,16 @@
 import react from 'React';
-import { Flatlist } from 'react-native';
+import { Flatlist, Text } from 'react-native';
+import { useSelector } from 'react-redux';
 
-const ProductsOverviewScreen = props => {
-  return <Flatlist/>;
-}
+const ProductsOverviewScreen = (props) => {
+  const products = useSelector((state) => state.products.availableProducts);
+  return (
+    <Flatlist
+      data={products}
+      keyExtractor={(item) => item.id}
+      renderItem={(itemData) => <Text>{itemData.item.title}</Text>}
+    />
+  );
+};
 
 export default ProductsOverviewScreen;
