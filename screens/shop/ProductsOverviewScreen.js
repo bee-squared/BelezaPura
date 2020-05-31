36373d80eee144e4f258333eac4c9be827031a6f
Brian Bouchard <b.bouch@gmail.com>
2020-05-30 21:58:08 -0600

    Add Product Item Component with Simple Styling

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { FlatList, Text, View } from 'react-native';
 import { useSelector } from 'react-redux';
 
+import ProductItem from '../../components/shop/ProductItem';
+
 const ProductsOverviewScreen = (props) => {
   const products = useSelector((state) => {
     return state.products.availableProducts;
@@ -9,14 +11,16 @@ const ProductsOverviewScreen = (props) => {
   return (
     <FlatList
       data={products}
-      keyExtractor={item => item.id}
-      renderItem={itemData => <View><Text>{itemData.item.title}</Text></View>}
+      keyExtractor={(item) => item.id}
+      renderItem={(itemData) => (
+        <ProductItem image={itemData.item.imageUrl} title={itemData.item.title} price={itemData.item.price} onViewDetail={() => {}} onAddToCart={() => {}}/>
+      )}
     />
   );
 };
 
 ProductsOverviewScreen.navigationOptions = {
-  headerTitle: 'All Products'
-}
+  headerTitle: 'All Products',
+};
 
 export default ProductsOverviewScreen;
