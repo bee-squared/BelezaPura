586a98cf4aac7b3972f357f4fc352368b57a1a77
Brian Bouchard <b.bouch@gmail.com>
2020-06-01 17:09:39 -0600

    Add touchable opacity experience for user on Android, retain border radius for product overview on Android

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {
   TouchableOpacity,
+  TouchableNativeFeedback, // for android
+  Platform,
   View,
   Text,
   Image,
@@ -11,30 +13,41 @@ import {
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 const ProductItem = (props) => {
+  let TouchableCmp = TouchableOpacity;
+
+  if (Platform.OS === 'android' && Platform.Version >= 21) {
+    TouchableCmp = TouchableNativeFeedback;
+  }
+
   return (
-    <TouchableOpacity onPress={props.onViewDetail}>
-      <View style={styles.product}>
-        <View style={styles.imageContainer}>
-          <Image style={styles.image} source={{ uri: props.image }} />
-        </View>
-        <View style={styles.details}>
-          <Text style={styles.title}>{props.title}</Text>
-          <Text style={styles.price}>${props.price.toFixed(2)}</Text>
-        </View>
-        <View style={styles.actions}>
-          <Button
-            color={Colors.primary}
-            title='View Details'
-            onPress={props.onViewDetail}
-          />
-          <Button
-            color={Colors.primary}
-            title='To Cart'
-            onPress={props.onAddToCart}
-          />
-        </View>
+    // TouchableNativeFeedback useForeground is applicable to android only, and it allows the ripple affect to apply to the entire container, not just the element that is pressed
+    <View style={styles.product}>
+      <View style={styles.touchable}>
+        <TouchableCmp onPress={props.onViewDetail} useForeground>
+          <View>
+            <View style={styles.imageContainer}>
+              <Image style={styles.image} source={{ uri: props.image }} />
+            </View>
+            <View style={styles.details}>
+              <Text style={styles.title}>{props.title}</Text>
+              <Text style={styles.price}>${props.price.toFixed(2)}</Text>
+            </View>
+            <View style={styles.actions}>
+              <Button
+                color={Colors.primary}
+                title='View Details'
+                onPress={props.onViewDetail}
+              />
+              <Button
+                color={Colors.primary}
+                title='To Cart'
+                onPress={props.onAddToCart}
+              />
+            </View>
+          </View>
+        </TouchableCmp>
       </View>
-    </TouchableOpacity>
+    </View>
   );
 };
 
@@ -49,6 +62,11 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     height: 300,
     margin: 20,
+  },
+  // for android to allow the product to retain border radius
+  touchable: {
+    borderRadius: 10,
+    overflow: 'hidden',
   },
   imageContainer: {
     width: '100%',
