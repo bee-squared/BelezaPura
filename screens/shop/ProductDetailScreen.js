33438f9dfc8ed409b8239d6bd5a6f50e7766b23f
Brian Bouchard <b.bouch@gmail.com>
2020-06-02 21:36:09 -0600

    Add scroll view to product detail and add price and description

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -11,23 +11,26 @@ import { useSelector } from 'react-redux';
 
 const ProductDetailScreen = (props) => {
   const productId = props.navigation.getParam('productId');
-  const selectedProduct = useSelector(state => state.products.availableProducts.find(prod => prod.id === productId))
+  const selectedProduct = useSelector((state) =>
+    state.products.availableProducts.find((prod) => prod.id === productId)
+  );
 
   return (
-    <View>
-      <Text>{selectedProduct.title}</Text>
-    </View>
+    <ScrollView>
+      <Image source={{ uri: selectedProduct.imageUrl }} />
+      <Button title="Add to Cart" onPress={() => {}}/>
+      <Text>${selectedProduct.price.toFixed(2)}</Text>
+      <Text>{selectedProduct.description}</Text>
+    </ScrollView>
   );
 };
 
-ProductDetailScreen.navigationOptions = navData => {
+ProductDetailScreen.navigationOptions = (navData) => {
   return {
-    headerTitle: navData.navigation.getParam('productTitle')
-  }
-}
+    headerTitle: navData.navigation.getParam('productTitle'),
+  };
+};
 
-const styles = StyleSheet.create({
-
-})
+const styles = StyleSheet.create({});
 
 export default ProductDetailScreen;
