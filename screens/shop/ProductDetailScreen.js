81f4080bf91fe1f46b0481ee46c4462ff7ff2a52
Brian Bouchard <b.bouch@gmail.com>
2020-06-06 12:13:27 -0600

    Style button on product detail screen

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -19,7 +19,9 @@ const ProductDetailScreen = (props) => {
   return (
     <ScrollView>
       <Image style={styles.image} source={{ uri: selectedProduct.imageUrl }} />
-      <Button color={Colors.primary} title='Add to Cart' onPress={() => {}} />
+      <View style={styles.actions}>
+        <Button color={Colors.primary} title='Add to Cart' onPress={() => {}} />
+      </View>
       <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
       <Text style={styles.description}>{selectedProduct.description}</Text>
     </ScrollView>
@@ -36,6 +38,10 @@ const styles = StyleSheet.create({
   image: {
     width: '100%',
     height: 300,
+  },
+  actions: {
+    marginVertical: 10,
+    alignItems: 'center'
   },
   price: {
     fontSize: 20,
