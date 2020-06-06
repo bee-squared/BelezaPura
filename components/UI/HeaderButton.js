1eb6025a73198c1a22f49d0988da96605f3e1a95
Brian Bouchard <b.bouch@gmail.com>
2020-06-06 14:36:41 -0600

    Add cart icon to screen header

--- components/UI/HeaderButton.js
+++ components/UI/HeaderButton.js
@@ -6,7 +6,7 @@ import { Ionicons } from '@expo/vector-icons';
 import Colors from '../../constants/Colors';
 
 const CustomHeaderButton = (props) => {
-  return <HeaderButton {...props} IconComponent={Ionicons} iconSize={23} color={Platform === 'android' ? 'white' : Colors.primary}/>;
+  return <HeaderButton {...props} IconComponent={Ionicons} iconSize={23} color={Platform.OS === 'android' ? 'white' : Colors.primary}/>;
 };
 
 export default CustomHeaderButton;
