d5114b88e90b295ddcc71bca4009581597cacb67
Brian Bouchard <b.bouch@gmail.com>
2020-06-15 09:06:47 -0600

    Add front end form to allow user to input new product details

--- screens/user/EditProductsScreen.js
+++ screens/user/EditProductsScreen.js
@@ -1,16 +1,70 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, ScrollView, Text, TextInput, StyleSheet } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+
+import HeaderButton from '../../components/UI/HeaderButton';
 
 const EditProductScreen = (props) => {
   return (
-    <View>
-      <Text>The Edit Product Screen</Text>
-    </View>
-  )
-}
+    <ScrollView>
+      <View style={styles.form}>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Title</Text>
+          <TextInput style={styles.input} />
+        </View>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Image URL</Text>
+          <TextInput style={styles.input} />
+        </View>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Price</Text>
+          <TextInput style={styles.input} />
+        </View>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Description</Text>
+          <TextInput style={styles.input} />
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+EditProductScreen.navigationOptions = (navData) => {
+  return {
+    headerTitle: navData.navigation.getParam('productId')
+      ? 'Edit Product'
+      : 'Add Product',
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title='Save'
+          iconName={Platform.OS === 'android' ? 'md-checkmark' : 'ios-checkmark'}
+          onPress={() => {
+            
+          }}
+        />
+      </HeaderButtons>
+    ),
+  };
+};
 
 const styles = StyleSheet.create({
-
+  form: {
+    margin: 20,
+  },
+  formControl: {
+    width: '100%',
+  },
+  label: {
+    fontFamily: 'open-sans-bold',
+    marginVertical: 8,
+  },
+  input: {
+    paddingHorizontal: 2,
+    paddingVertical: 5,
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+  },
 });
 
 export default EditProductScreen;
