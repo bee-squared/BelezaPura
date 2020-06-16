21feed8e9f3db020b6dee2408ea4aaba4603b302
Brian Bouchard <b.bouch@gmail.com>
2020-06-16 11:28:07 -0600

    Refactor project to use reusable Card component for all components using card styling

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -9,6 +9,8 @@ import {
   StyleSheet,
 } from 'react-native';
 
+import Card from '../UI/Card';
+
 const ProductItem = (props) => {
   let TouchableCmp = TouchableOpacity;
 
@@ -18,7 +20,7 @@ const ProductItem = (props) => {
 
   return (
     // TouchableNativeFeedback useForeground is applicable to android only, and it allows the ripple affect to apply to the entire container, not just the element that is pressed
-    <View style={styles.product}>
+    <Card style={styles.product}>
       <View style={styles.touchable}>
         <TouchableCmp onPress={props.onSelect} useForeground>
           <View>
@@ -29,25 +31,16 @@ const ProductItem = (props) => {
               <Text style={styles.title}>{props.title}</Text>
               <Text style={styles.price}>${props.price.toFixed(2)}</Text>
             </View>
-            <View style={styles.actions}>
-              {props.children}
-            </View>
+            <View style={styles.actions}>{props.children}</View>
           </View>
         </TouchableCmp>
       </View>
-    </View>
+    </Card>
   );
 };
 
 const styles = StyleSheet.create({
   product: {
-    shadowColor: 'black',
-    shadowOpacity: 0.26,
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 8,
-    elevation: 5, // for android
-    borderRadius: 10,
-    backgroundColor: 'white',
     height: 300,
     margin: 20,
   },
